8871d380a81b8c5eea8eb3730bed44ce27ce3b7a
donghun93 <alswn4516@gmail.com>
2022-10-19 13:36:58 +0900

    JPA Auditing 기능 추가
    - BaseTimeEntity 등록일, 수정일 필드 구성
    - BaseEntity는 BaseTimeEntity를 상속받은 후 등록자, 수정자 필드 구성

---
+++ src/main/java/com/devwinter/supermarket/config/JpaAuditing.java
@@ -0,0 +1,9 @@
+package com.devwinter.supermarket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditing {
+}
