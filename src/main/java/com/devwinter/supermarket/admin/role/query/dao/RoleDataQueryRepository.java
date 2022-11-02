14a1b83cbdf3cdd4a01b65a0986475ac01abc38b
donghun93 <alswn4516@gmail.com>
2022-11-02 11:33:29 +0900

    #15 - 권한 상세 조회용 쿼리 리포지토리 생성
    - 권한 상세 조회 쿼리 리포지토리 생성

--- src/main/java/com/devwinter/supermarket/admin/role/query/dao/RoleDataQueryRepository.java
+++ src/main/java/com/devwinter/supermarket/admin/role/query/dao/RoleDataQueryRepository.java
@@ -1,9 +1,13 @@
 package com.devwinter.supermarket.admin.role.query.dao;
 
+import com.devwinter.supermarket.admin.role.query.response.RoleDetailItemResponse;
 import com.devwinter.supermarket.admin.role.query.response.RoleListItemResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface RoleDataQueryRepository {
     Page<RoleListItemResponse> getRoleList(Pageable pageable);
+    Optional<RoleDetailItemResponse> getRoleDetail(Long roleId);
 }
--- src/main/java/com/devwinter/supermarket/admin/role/query/dao/RoleDataQueryRepositoryImpl.java
+++ src/main/java/com/devwinter/supermarket/admin/role/query/dao/RoleDataQueryRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.devwinter.supermarket.admin.role.query.dao;
 
 import com.devwinter.supermarket.admin.role.command.domain.QRole;
 import com.devwinter.supermarket.admin.role.command.domain.Role;
+import com.devwinter.supermarket.admin.role.query.response.RoleDetailItemResponse;
 import com.devwinter.supermarket.admin.role.query.response.RoleListItemResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Wildcard;
@@ -14,6 +15,7 @@ import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.devwinter.supermarket.admin.role.command.domain.QRole.role;
 import static com.querydsl.core.types.dsl.Wildcard.count;
@@ -27,8 +29,7 @@ public class RoleDataQueryRepositoryImpl implements RoleDataQueryRepository {
     @Override
     public Page<RoleListItemResponse> getRoleList(Pageable pageable) {
         List<RoleListItemResponse> fetch = queryFactory
-                .select(Projections.constructor(RoleListItemResponse.class,
-                        role.id, role.name, role.desc, role.createdBy, role.createdDate, role.modifiedDate))
+                .select(Projections.constructor(RoleListItemResponse.class, role.id, role.name, role.desc))
                 .from(role)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -39,5 +40,16 @@ public class RoleDataQueryRepositoryImpl implements RoleDataQueryRepository {
                 .from(role);
 
         return PageableExecutionUtils.getPage(fetch, pageable, countQuery::fetchOne);
+    }
+
+    @Override
+    public Optional<RoleDetailItemResponse> getRoleDetail(Long roleId) {
+
+        return Optional.ofNullable(queryFactory
+                .select(Projections.constructor(RoleDetailItemResponse.class,
+                        role.id, role.name, role.desc, role.createdBy, role.createdDate))
+                .from(role)
+                .where(role.id.eq(roleId))
+                .fetchOne());
     }
 }
